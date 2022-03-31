458cda1280b35d2a2d6f1e4511754364f902a82f
mahesh8492 <97870847+mahesh8492@users.noreply.github.com>
2022-03-31 16:27:12 +0530

    Update HelloRestController.java

--- src/main/java/com/zensar/HelloRestController.java
+++ src/main/java/com/zensar/HelloRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloRestController {
 	
 	@GetMapping("/greet/{name}")
 	public String sayHello(@PathVariable(name="name") String name) {
