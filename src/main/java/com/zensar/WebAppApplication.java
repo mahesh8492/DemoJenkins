3addd6efbc9ec56aed46d71990cfdf690bf82200
mahesh8492 <97870847+mahesh8492@users.noreply.github.com>
2022-04-04 12:42:27 +0530

    Add files via upload

--- src/main/java/com/zensar/WebAppApplication.java
+++ src/main/java/com/zensar/WebAppApplication.java
@@ -3,7 +3,7 @@ package com.zensar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zensar")
 public class WebAppApplication {
 
 	public static void main(String[] args) {
